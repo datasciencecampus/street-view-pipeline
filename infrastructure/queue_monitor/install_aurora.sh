6f2ad570d2c963b0b5ff11b0fa71abe9516eddf7
Phil Stubbings <phil@parasec.net>
2018-09-14 14:22:57 +0100

    update aurora version

--- infrastructure/queue_monitor/install_aurora.sh
+++ infrastructure/queue_monitor/install_aurora.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+VERSION="aurora_linux_amd64_v2.1.tar.gz"
 if [ ! -e aurora ] ;then
-  wget https://github.com/Luxurioust/aurora/releases/download/2.0/aurora_linux_amd64_v2.0.tar.gz
-  tar -zxvf aurora_linux_amd64_v2.0.tar.gz
-  rm -f aurora_linux_amd64_v2.0.tar.gz
+  wget https://github.com/Luxurioust/aurora/releases/download/2.1/$VERSION
+  tar -zxvf $VERSION
+  rm -f $VERSION
 fi
