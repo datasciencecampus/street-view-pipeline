2f642c1447152cb1ea0996e3f319e5abdbb91bbc
Phil Stubbings <phil@parasec.net>
2018-09-18 10:51:11 +0100

    take (quota - queue_size) image jobs from api

--- data_load/batch_loader.py
+++ data_load/batch_loader.py
@@ -9,33 +9,55 @@ sys.path.append('../api')
 from api import API
 
 
-def push_jobs(jobs):
-    """push jobs to image_download queue."""
-    dst_tube = 'image_download_jobs'
-    beanstalk = Connection(host='localhost', port=11300)
+def connect(host, port, dst_tube):
+    """connect to beanstalk, use dst_tube tube."""
+    beanstalk = Connection(host, port))
     print("tubes:", beanstalk.tubes())
     print("switching to", beanstalk.use(dst_tube))
     print("now using", beanstalk.using())
+    return beanstalk
+
+
+def push_jobs(jobs, beanstalk):
+    """push jobs to beanstalk. assume already using tube."""
+    if beanstalk.using() == 'default':
+        print("warning: using default tube.")
 
     for job in jobs:
         job_json = json.dumps(job)
         beanstalk.put(job_json)        
-        print("pushed {}_{}_{}_{}".format(job['city'], job['osm_way_id'], job['sequence'], job['cam_dir']))
+        print("pushed {}_{}_{}_{}".format(job['city'], job['osm_way_id'],
+                                          job['sequence'], job['cam_dir']))
     
-    beanstalk.close()
     return len(jobs) 
 
 
-def transfer_jobs(limit=25000):
-    """get all jobs prioritised by sampling order,
-    then push to download queue."""
-    job_api = API()
-    ok, jobs = job_api.all_jobs(limit)
-    if not ok:
-        return -1
-    return push_jobs(jobs)
+def existing_jobs(beanstalk, dst_tube):
+    """get existing number of jobs on dst_tube."""
+    stats = beanstalk.stats_tube(dst_tube)
+    return stats['current-jobs-ready']
+
+
+def transfer_jobs(limit, host, port, dst_tube):
+    """get all jobs prioritised by sampling order, then push to download
+    queue."""
+    beanstalk = connect(host, port, dst_tube)
+
+    # only take what we need.
+    limit = max(0, limit - existing_jobs(beanstalk, dst_tube))
+
+    try:
+        job_api = API()
+        ok, jobs = job_api.all_jobs(limit)
+
+        if not ok:
+            return -1
+        return push_jobs(jobs)
+
+     finally:
+         beanstalk.close()
 
 
 if __name__ == '__main__':
     limit = int(sys.argv[1])
-    transfer_jobs(limit)
+    transfer_jobs(limit, 'localhost', 11300, 'image_download_jobs')
