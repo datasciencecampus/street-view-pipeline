6335eab92bcca87190fe9884f4bcb1b216b97560
Phil Stubbings <phil@parasec.net>
2018-09-18 16:49:52 +0100

    start plugins

--- image_processing/image_processor.py
+++ image_processing/image_processor.py
@@ -3,7 +3,6 @@ import logging
 from pystalkd.Beanstalkd import Connection
 
 import sys
-import subprocess
 import json
 
 sys.path.append('../generic')
@@ -20,16 +19,16 @@ class Processor(Consumer):
         self.logger = logging.getLogger(__name__)
         self.worker_name = worker_name
         self.beanstalk = Connection(host=beanstalk_host, port=beanstalk_port)
-        self.src_store = src_store 
+        self.src_store = src_store
         self.sample_api = API()
-        self.green = Green()       
- 
+        self.green = Green() # <---
+
     def process_job(self, json_job):
         job_dict = json.loads(json_job)
-        filename = '{}/{city}/{road_name}/{osm_way_id}/{cam_dir}/{sequence}_{latitude}_{longitude}_{id}.jpg'.format(src_store, **job_dict) 
+        filename = '{}/{city}/{road_name}/{osm_way_id}/{cam_dir}/{sequence}_{latitude}_{longitude}_{id}.jpg'.format(src_store, **job_dict)
         sample_id = job_dict['id']
-        v = self.green.green_from_file(filename)
-        self.logger.info("green: {:.4f}".format(v))
+        v = self.green.green_from_file(filename) # <---
+        self.logger.info("vegetation: {:.4f}".format(v))
         return self.sample_api.sample(sample_id, v)
 
 
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     worker_name = sys.argv[1]
     max_jobs = int(sys.argv[2])
     src_queue = sys.argv[3]
-    src_store = sys.argv[4] 
+    src_store = sys.argv[4]
 
     processor = Processor(worker_name, 'localhost', 11300, src_store)
     processor.consume(max_jobs, src_queue)
